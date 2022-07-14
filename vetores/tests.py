3c288c349ad79baa21bebdb1ddeda0e051e0535a
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-07-15 00:06:46 +0100

    finished verore ex1

--- vetores/tests.py
+++ vetores/tests.py
@@ -1,8 +1,35 @@
 from django.test import TestCase
 
 from random import random, randrange
-n=1,9,3
+# n=1,9,3
+#
+# print(randrange(11))
+# print(randrange(11))
+'''
+tabela = [ int(input("Informe um valor inteiro, faltam "+str(12-x)+"\n"))
+           for x in range(12)]
 
-print(randrange(11))
-print(randrange(11))
+tabela = [ int(random()*100-30)
+           for x in range(12)]
 
+print("Eis os doze valores" )
+
+print( [ '{0:3d}'.format(x) for x in tabela] )
+
+primeiro = int(input("Informe o X entre 1 e 12"))
+
+segundo = int(input("Informe o Y também entre 1 e 12"))
+
+print("O elemento",primeiro,"vale",tabela[primeiro-1])
+
+print("O elemento",segundo,"vale",tabela[segundo-1])
+
+print("A soma é",tabela[primeiro-1]+tabela[segundo-1])
+'''
+
+data = []
+n = int(input('Enter how many elements you want: '))
+for i in range(0, 3):
+    x = input('Enter the numbers into the array: ')
+    data.append(x)
+print(data)
--- vetores/views.py
+++ vetores/views.py
@@ -5,8 +5,47 @@ from random import random, randrange
 def home(request):
     return render(request, 'home.html')
 
+# def ex1(request):
+#     y = randrange(11)
+#     x = randrange(11)
+#     s = x + y
+#     return render(request, 'ex1.html', {'x': x, 'y': y, 's': s})
+
+# def ex1(request):
+#     x = None
+#     y = None
+#     s = None
+#     data = None
+#     if request.method == "POST":
+#         data = int(request.POST.get('valueuser'))
+#
+#         if data.count() > 3:
+#
+#             y = randrange(data)
+#             x = randrange(data)
+#             s = x + y
+#     return render(request, 'ex1.html', {'x': x, 'y': y, 's': s, 'data':data})
+
+
 def ex1(request):
-    y = randrange(11)
-    x = randrange(11)
-    s = x + y
-    return render(request, 'ex1.html', {'x': x, 'y': y, 's': s})
+    list_value = None
+    x = None
+    y = None
+    x_p = None
+    y_p = None
+    sum = None
+
+    if request.method == "POST":
+        value_1 = int(request.POST.get('value_1'))
+        value_2 = int(request.POST.get('value_2'))
+        value_3 = int(request.POST.get('value_3'))
+        value_4 = int(request.POST.get('value_4'))
+        x = int(request.POST.get('value_x'))
+        y = int(request.POST.get('value_y'))
+        list_value = [value_1, value_2, value_3, value_4]
+        x_p = list_value[x]
+        y_p = list_value[y]
+        sum = list_value[x] + list_value[y]
+    return render(request, 'ex1.html', {'list_value': list_value,
+                                        'x': x, 'y': y, 'x_p': x_p,
+                                        'y_p': y_p, 'sum': sum})
