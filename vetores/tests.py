f6b363ac42730bd21821b0d31b6e16cb36ccf3a6
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-07-17 18:50:33 +0100

    Finished execise 5

--- vetores/tests.py
+++ vetores/tests.py
@@ -1,10 +1,25 @@
-a = ['1', ' 2', ' 3', ' 4', ' 9']
+# a = ['1', ' 2', ' 3', ' 4', ' 9']
+#
+# b = []
+#
+# for i in a:
+#     c = i.replace(' ', '')
+#     b.append(c)
+#
+# print(b)
 
-b = []
 
+a = ['1', '2', '3', '4', '9', '8', '16', '15']
+a_int = []
+
+count = 0
 for i in a:
-    c = i.replace(' ', '')
-    b.append(c)
+    a_int.append(int(i))
 
-print(b)
+for i in a_int:
+    if i%2 == 0:
+        count += 1
 
+print(a_int)
+
+print(count)
--- vetores/urls.py
+++ vetores/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, ex1, ex2, ex3, ex4
+from .views import home, ex1, ex2, ex3, ex4, ex5
 
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('ex2', ex2, name='ex2_x'),
     path('ex3', ex3, name='ex3_x'),
     path('ex4', ex4, name='ex4_x'),
+    path('ex5', ex5, name='ex5_x'),
 ]
