111ed8212565f15bbd69d33a2bb0693ef11568ab
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-07-17 17:38:07 +0100

    Finished exercise 4

--- vetores/tests.py
+++ vetores/tests.py
@@ -1,11 +1,10 @@
+a = ['1', ' 2', ' 3', ' 4', ' 9']
 
-a = ['1', '2', '3', '4', '5', '6']
+b = []
 
-b = [a[3], a[4], a[5], a[0], a[1], a[2], ]
+for i in a:
+    c = i.replace(' ', '')
+    b.append(c)
 
-c = int(len(a)/2)
+print(b)
 
-if len(a) % 2 == 1:
-    print(False)
-
-print([a[c:], a[:c]])
